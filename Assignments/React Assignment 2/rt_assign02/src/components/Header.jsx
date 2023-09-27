c43e5c13ff40bb6ed1bb92fe0804e89fbe6bf413
IrfanChatha <imchatha@hotmail.com>
2023-09-27 16:35:26 +0500

    React Assignment 2 Updated

--- Assignments/React Assignment 2/rt_assign02/src/components/Header.jsx
+++ Assignments/React Assignment 2/rt_assign02/src/components/Header.jsx
@@ -1,31 +1,28 @@
 import React from 'react'
 
-
 const Header = () => {
   return (
-    <div>
-
-
+    <div style={{textAlign: 'center'}}>
 
 <img
     src={require('../assets/images/Heading.png')}
     alt="Sir Umair Azmat"
-    className={"headerStyle"}
-    // style={{
-    //     width: '300px', /* Set your desired width */
-    //     height: 'auto', /* Maintain the aspect ratio */
-    //     margin: '10px', /* Add margin around the image */
-    //     alignText: 'center' /* Add more styling properties as needed */
-    // }}
+    style={{
+      width: '300px',
+      height: 'auto',
+      margin: '10px',
+      textAlign: 'center'
+    }}
 />
         { <nav>
-             <a href="index.html">Home</a>
-             <a href="menu.html">Menu</a>
-            <a href="aboutus.html">About</a>
-            <a href="timing.html">Timings</a>
-            <a href="contactus.html">Contact Us</a>  
+            <a href="index.html" style={{paddingRight:'10px'}}>Home</a>
+            <a href="menu.html" style={{paddingRight:'10px'}}>Menu</a>
+            <a href="aboutus.html" style={{paddingRight:'10px'}}>About</a>
+            <a href="timing.html" style={{paddingRight:'10px'}}>Timings</a>
+            <a href="contactus.html" style={{paddingRight:'1px'}}>Contact Us</a>  
         </nav> }
 
+<hr/>
 
     </div>
   )
