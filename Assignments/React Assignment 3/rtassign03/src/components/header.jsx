02c88dd11a089af19c0e8b7a2510dc2f62dcb28a
IrfanChatha <imchatha@hotmail.com>
2023-10-05 18:40:40 +0500

    React Assignment 3 Updated

--- Assignments/React Assignment 3/rtassign03/src/components/header.jsx
+++ Assignments/React Assignment 3/rtassign03/src/components/header.jsx
@@ -5,28 +5,26 @@ import search_icon from './assets/images/search_icon.png';
 
 function Header() {
   return (
-
 <>
-
 <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
-    <img src={logo} alt='Search'/><h3>TROPICO</h3>
+    <img src={logo} alt='Search'/><h3 style={{paddingLeft:'10px'}}>TROPICO</h3>
     <div className="collapse navbar-collapse justify-content-end fs-5 text" id="navbarSupportedContent">
       <ul className="navbar-nav mb-lg-0 mx-5">
         <li className="nav-item me-2 px-3">
-          <a className="nav-a active" aria-current="page" path="#"><b>Home</b></a>
+          <a className="nav-a" aria-current="page" path="#"><b>Home</b></a>
         </li>
         <li className="nav-item me-2 px-3">
-          <a className="nav-a active" aria-current="page" path="#"><b>Fruits</b></a>
+          <a className="nav-a" aria-current="page" path="#"><b>Fruits</b></a>
         </li>
         <li className="nav-item me-2 px-3">
-          <a className="nav-a active" aria-current="page" path="#"><b>Services</b></a>
+          <a className="nav-a" aria-current="page" path="#"><b>Services</b></a>
         </li>
         <li className="nav-item me-2 px-3">
-          <a className="nav-a active" aria-current="page" path="#"><b>Contact Us</b></a>
+          <a className="nav-a" aria-current="page" path="#"><b>Contact Us</b></a>
         </li>
       </ul>
       <form className="d-flex " role="search">
@@ -36,41 +34,6 @@ function Header() {
     </div>
   </div>
 </nav>
-
-
-
-
-{/* <div style={{
-        fontFamily: 'Verdana',
-        fontSize: '20px',
-        border: '0px',
-        backgroundColor: 'rgb(243, 233, 233)',
-        textAlign: 'center',
-        paddingLeft: '20px'
-      }}>
-<nav>
-            <table>
-                <tr> 
-                    <td width="50px"><img src={logo} alt='Logo'/></td>
-                    <td width="150px"><b>TROPIKO</b></td>
-                    <td width="450px"></td>
-                    <td width="100px"><a href="index.html">Home</a></td>
-                    <td width="100px"><a href="menu.html">Fruits</a></td>
-                    <td width="120px"><a href="aboutus.html">Services</a></td>
-                    <td width="150px"><a href="timing.html">Contact Us</a></td>
-                    <td width="50px"><img src={search_icon} alt='Search'/></td>
-                    <td width="180px"><a href="menu.html">
-                    <button style={{fontSize:'18px',
-                        fontWeight: 'bold',
-                        color:'white',
-                        backgroundColor:'orangered',
-                        border:'0px',
-                        padding:'9px 14px'
-                    }}>GET A QUOTE</button></a> </td>
-                </tr>
-            </table>
-    </nav>
-    </div> */}
 
 </>
   )
--- Assignments/React Assignment 3/rtassign03/src/components/pages/landingPage.jsx
+++ Assignments/React Assignment 3/rtassign03/src/components/pages/landingPage.jsx
@@ -1,10 +1,41 @@
 import React, { Fragment } from 'react';
-// import carditem1 from '../assets/images/carditem1.png';
-// import carditem2 from '../assets/images/carditem2.png';
+import slideimage from '../assets/images/slideimage.png';
 
 const LandingPage = () => {
   return (
 <Fragment>
+
+<div className="container-fluid">
+   <div className="row">
+    <div className="col-8 bg-body-tertiary" style={{width:'50%', backgroundColor: 'rgb(243, 233, 233)', paddingLeft:'20px'}}>
+    <h1 className="mt-6" style={{paddingTop:'10%', fontFamily:'Arial', fontWeight:'bold', fontSize:'5vh'}}>Welcome to <br/> Our Fruits Shop</h1>
+    <p style={{fontFamily:'Arial', fontSize:'20px'}}>There are many variations of passages of Lorem Ipsum, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
+    <button style={{fontSize:'18px',
+    fontWeight: 'bold',
+    color:'white',
+    backgroundColor:'orangered',
+    border:'0px',
+    padding:'9px 14px'}}>SHOP NOW</button>
+    <button style={{fontSize:'18px',
+      fontWeight:'bold',
+      color:'white',
+      backgroundColor:'black',
+      border:'0px',
+      padding:'9px 14px',
+      marginLeft:'10px'}}>CONTACT US</button>
+        
+        </div>
+    
+    <div className="col-6 bg-body-tertiary" style={{width:'50%', backgroundColor: 'rgb(243, 233, 233)', paddingLeft:'20px'}}>
+    <img src={slideimage} alt='_Image' className="img-fluid"/>
+    </div>
+  </div>
+</div>
+
+
+
+
+
 
 <h1 style={{fontFamily:'Arial', fontWeight:'bold', fontSize:'30px', textAlign:'center', paddingTop:'350px', marginBottom:'-10px'}}>Our Services</h1>
 <p style ={{textAlign:'center', fontFamily:'Arial'}}>There are many variations of passages of Lorem Ipsum available, but the majority have</p>
