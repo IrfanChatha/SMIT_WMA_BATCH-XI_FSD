e4fa864d2d1e8e3af1318837d07f42b9d01e8a81
IrfanChatha <imchatha@hotmail.com>
2023-10-10 23:18:44 +0500

    Hackathon Project Added

--- Hackathon Task 081023/SMSYS/smsysapp/src/App.js
+++ Hackathon Task 081023/SMSYS/smsysapp/src/App.js
@@ -1,13 +1,14 @@
 import React from "react"
 import Index from "./Components/Routes"
 import "bootstrap/dist/js/bootstrap.bundle"
+import SideNavBar from "./Components/Navbar/SideNavBar"
 
 function App() {
 
   return (
     <div>
  <Index />
-
+{/* <SideNavBar /> */}
  
 
     </div>
---
+++ Hackathon Task 081023/SMSYS/smsysapp/src/Components/Navbar/SideBar.jsx
@@ -0,0 +1,40 @@
+// import React from 'react'
+// import './SideBar.css'
+
+// const SideBar = () => {
+//   return (
+//     <div>
+
+// <div className="main-container dFlex">
+// <div className="sideBar side_nav">
+// <div className="header-box px-2 pt-3 pb-4">
+//     <h1 className="fs-4"><span className="bgWhite textGreen rounded custom-shadow px-2 me-2">SMIT</span><span className="textWhite">Hackathon</span></h1>
+// </div> 
+
+// <ul className="listUnstyled px-5">
+//     <li><a href="#" className="textDecoration:'none' textWhite px-3 py-2 d-block">Dashboard</a></li>
+//     <li className="textDecoration:'none' textWhite px-3 py-2 d-block">Students</li>
+//     <li className="textDecoration:'none' textWhite px-3 py-2 d-block">Courses</li>
+//     <li className="textDecoration:'none' textWhite px-3 py-2 d-block">Attendance</li>
+// </ul>
+//     <hr className="h-color mx-3" />
+// </div>
+
+// <div className="content">
+// <div className="row text-bg-primary p-3"> <h2 style={{color:'white'}}>Dashboard</h2></div>
+
+// </div>
+
+
+// </div>
+
+
+
+
+
+
+//     </div>
+//   )
+// }
+
+// export default SideBar
