f5b3d4f67da96268b22bff2b2e772d4cf3075d36
IrfanChatha <imchatha@hotmail.com>
2023-10-25 17:29:55 +0500

    NextJS Initial Commit

---
+++ React Native/ExpoApp/AwesomeProject/App.js
@@ -0,0 +1,28 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+// const textStyle = StyleSheet.create({
+//   hellotxt: {
+//     backgroundColor: 'green',
+//     fontSize: '40px',
+//     color: 'white',
+//   },
+// });
