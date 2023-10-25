ecf4555b43b8b82fe0fd3094613431d35a9e90cd
IrfanChatha <imchatha@hotmail.com>
2023-10-26 01:04:38 +0500

    NextJS Initial Commit

--- NextJS13/FirstNextApp/mynextapp/app/page.js
+++ NextJS13/FirstNextApp/mynextapp/app/page.js
@@ -1,9 +1,21 @@
-function Home() {
+import React from "react";
+
+const Home = () => {
   return (
-    <div>
-      <h1>Hello World - Next JS</h1>
+    <div className="flex items-center justify-center h-screen">
+      <div
+        className="text-white"
+        style={{
+          backgroundColor: "#1a237e",
+          padding: "1rem",
+          borderRadius: "10px",
+          boxShadow: "0px 4px 6px blue",
+        }}
+      >
+        Hello World! My NextJS App
+      </div>
     </div>
   );
-}
+};
 
 export default Home;
