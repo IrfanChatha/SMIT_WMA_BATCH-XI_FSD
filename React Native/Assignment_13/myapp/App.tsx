4046af3c62ee2eb2ab73a05b12d976192e598ffc
IrfanChatha <imchatha@hotmail.com>
2023-11-04 01:26:00 +0500

    Updated Assignment 13

--- React Native/Assignment_13/myapp/App.tsx
+++ React Native/Assignment_13/myapp/App.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
-import {View} from 'react-native';
-import {Text} from 'react-native';
+import {StyleSheet, Image} from 'react-native';
+import {LinearGradient} from 'react-native-linear-gradient';
+import owl from './components/assets/owl1.png';
 
-export default function App() {
+const App = () => {
   return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
-  );
+
+<LinearGradient colors={['rgba(102, 210, 204, 1)', 'rgba(48, 98, 243, 1)']} style={styles.linearGradient}>
+<Image source={owl} style={{ width: 130, height: 130}}/>
+</LinearGradient>
+
+  )
 }
+export default App
+
+var styles = StyleSheet.create({
+  linearGradient: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'}
+});
