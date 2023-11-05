c92976f1ce4d5ccf0e407bed726cfa298cead8b8
IrfanChatha <imchatha@hotmail.com>
2023-11-05 23:43:19 +0500

    Updated Assignment 13

--- React Native/Assignment_13/myapp/src/components/screens/startscreen1/Startscreen1.tsx
+++ React Native/Assignment_13/myapp/src/components/screens/startscreen1/Startscreen1.tsx
@@ -5,8 +5,9 @@ import TravelMono1 from '../../assets/TravelingMonochromatic1.png'
 const Startscreen1 = () => {
     return (
 
-        <View style={{ flex: 2 }}>
+        <View style={{ flex: 2, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white' }}>
             <Image source={TravelMono1} style={{ width: 200, height: 200 }} />
+            <Text>Start Screen 1</Text>
         </View>
 
     )
