10a39fa34c38421ce8b6da431ef252603fe22c02
IrfanChatha <imchatha@hotmail.com>
2023-11-06 00:58:50 +0500

    Updated Assignment 13

--- React Native/Assignment_13/myapp/src/components/screens/startscreen1/Startscreen1.tsx
+++ React Native/Assignment_13/myapp/src/components/screens/startscreen1/Startscreen1.tsx
@@ -1,15 +1,19 @@
 import { View, Image, Text } from 'react-native'
-import React from 'react'
+import { React } from 'react'
 import TravelMono1 from '../../assets/TravelingMonochromatic1.png'
 
 const Startscreen1 = () => {
     return (
 
-        <View style={{ flex: 2, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white' }}>
-            <Image source={TravelMono1} style={{ width: 200, height: 200 }} />
-            <Text>Start Screen 1</Text>
-        </View>
-
+        <>
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white' }}>
+                <Image source={TravelMono1} style={{ width: 250, height: 250, marginTop: '30%' }} />
+            </View>
+            <View style={{ flex: 1, alignItems: 'center', backgroundColor: 'white' }}>
+                <Text style={{ fontSize: 32, color: 'black', fontWeight: 'bold', textAlign: 'center' }}>Make your own private travel plan</Text>
+                <Text style={{ fontSize: 22, color: 'lightgray', textAlign: 'center', marginTop: '5%' }}>Formulate your strategy to receive wonderful gift packs</Text>
+            </View>
+        </>
     )
 }
 export default Startscreen1
