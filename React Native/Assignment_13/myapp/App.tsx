76e06bd6a4a6777eb6df075f71aa446c24e70435
IrfanChatha <imchatha@hotmail.com>
2023-11-07 21:54:39 +0500

    Updated Assignment 13

--- React Native/Assignment_13/myapp/App.tsx
+++ React Native/Assignment_13/myapp/App.tsx
@@ -4,7 +4,8 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Startscreen1 from './src/components/screens/startscreen1/Startscreen1';
 import Startscreen2 from './src/components/screens/startscreen2/Startscreen2';
 import Startscreen3 from './src/components/screens/startscreen3/Startscreen3';
-import SplashScreen from 'react-native-splash-screen';
+// import SplashScreen from 'react-native-splash-screen';
+import Splashscreen from './src/components/screens/splashscreen/Splashscreen';
 
 
 const Stack = createNativeStackNavigator();
@@ -12,7 +13,8 @@ const Stack = createNativeStackNavigator();
 function Navigation() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Screen_1">
+      <Stack.Navigator initialRouteName="SplashScreen">
+        <Stack.Screen name="Splash_Screen" component={Splashscreen} />
         <Stack.Screen name="Screen_1" component={Startscreen1} />
         <Stack.Screen name="Screen_2" component={Startscreen2} />
         <Stack.Screen name="Screen_3" component={Startscreen3} />
