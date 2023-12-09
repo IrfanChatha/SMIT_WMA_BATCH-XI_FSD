5c70c5f350c2962233e69b59cca7dfad081f940a
IrfanChatha <imchatha@hotmail.com>
2023-12-10 01:09:34 +0500

    Profile Website

---
+++ Final Hackathon 101223/hkapp/src/app/page.js
@@ -0,0 +1,11 @@
+import { useClient } from 'next/client';
+import Login from '@/pages/loginPage/loginPage'
+
+export default function Home() {
+  return (
+    <>
+   
+    <Login />
+    </>
+  )
+}
